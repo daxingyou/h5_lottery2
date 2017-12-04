dd2dc9a2ee857011a4c7bf48c01067ef23a871de
Sam <qianduan123@outlook>
2017-12-04 12:03:20 +0800

    hh

--- src/router/index.js
+++ src/router/index.js
@@ -39,7 +39,7 @@ import agent from '@/components/lobbyTemplate/agent'  // 大厅代理加盟
 import agentApply from '@/components/lobbyTemplate/agentApply'  // 大厅代理加盟注册页
 import info_data from '@/components/lobbyTemplate/info_data'  //个人中心-账户管理
 import notification from '@/components/lobbyTemplate/notification'  //个人中心-个人消息
-import join from '@/components/lobbyTemplate/join' //欢迎加入
+// import join from '@/components/lobbyTemplate/join' //欢迎加入
 import withdrawals_bind from '@/components/lobbyTemplate/withdrawals_bind'
 
 import about from '@/components/lobbyTemplate/about'  // 大厅 關於我們
@@ -168,7 +168,7 @@ export default new Router({
     { path: '/lobbyTemplate/agentApply', name:'agentApply', component: agentApply },
     { path: '/lobbyTemplate/info_data', name:'info_data', component: info_data },
     { path: '/lobbyTemplate/notification', name:'notification', component:notification },
-    { path: '/lobbyTemplate/join', name:'join', component:join },
+    // { path: '/lobbyTemplate/join', name:'join', component:join },
     { path: '/lobbyTemplate/withdrawals_bind', name:'withdrawals_bind', component:withdrawals_bind },
     { path: '/lobbyTemplate/promo', name:'promo', component:promo},
     { path: '/lobbyTemplate/promo_content', name: 'promo_content', component: promo_content }
