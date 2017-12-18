eec1d2e03f32c17d1cf633aaff9d66b3c18bfda3
Sam <qianduan123@outlook>
2017-12-18 17:21:22 +0800

    代理注册配置修改

--- src/components/lobbyTemplate/agent.vue
+++ src/components/lobbyTemplate/agent.vue
@@ -153,7 +153,7 @@
                                                 <div class="form_g password">
                                                     <legend>确认密码</legend>
                                                     <input type="password" placeholder="请输入6~20位密码" v-model="confirmpassword"  autocomplete="off" class="confirmpassword" @input="checkIsEqual('.confirmpassword')">
-                                                    <i class="eye  " @click="show('eye2')"></i>
+                                                    <i class="eye " @click="show('eye2')"></i>
                                                 </div>
                                                 <label class="error-message"></label>
                                             </fieldset>
@@ -161,7 +161,6 @@
                                                 <div class="form_g password">
                                                     <legend>确认密码</legend>
                                                     <input type="text" placeholder="请输入6~20位密码" v-model="confirmpassword"  autocomplete="off" class="confirmpassword" @input="checkIsEqual('.confirmpassword')">
-                                                    <i class="eye  " @click="show('eye2')"></i>>
                                                     <i class="eye active" @click="show('act2')"></i>
                                                 </div>
                                                 <label class="error-message"></label>
@@ -169,14 +168,15 @@
                                             <fieldset>
                                                 <div class="form_g account">
                                                     <legend>真实姓名</legend>
-                                                    <input type="text" placeholder="请输入您的真实姓名" v-model="relName">
+                                                    <input type="text" placeholder="请输入您的真实姓名" v-model="relName" class="relName"  @input="checkrealyName(relName,'relName')">
                                                     <i class="close cs3" @click="ClearInput('cs3','relName')"></i>
                                                 </div>
+                                                <label class="error-message"></label>
                                             </fieldset>
                                             <fieldset>
                                                 <div class="form_g account">
                                                     <legend>银行卡号</legend>
-                                                    <input type="tel" placeholder="请输入取款银行卡号" v-model="bankNum">
+                                                    <input type="tel" placeholder="请输入取款银行卡号" v-model="bankNum" class="bankNum" @input="checkBankNum(bankNum,'bankNum')">
                                                     <i class="close cs4" @click="ClearInput('cs4','bankNum')"></i>
                                                 </div>
                                                 <label class="error-message"></label>
@@ -194,7 +194,7 @@
                                             <fieldset>
                                                 <div class="form_g account">
                                                     <legend>开户行地址</legend>
-                                                    <input type="text" placeholder="如:北京市海淀区中关村支行" v-model="bankAdd">
+                                                    <input type="text" placeholder="如:北京市海淀区中关村支行" v-model="bankAdd" class="bankAdd" @input="checkBankAdd(bankAdd,'bankAdd')">
                                                     <i class="close cs5" @click="ClearInput('cs5','bankAdd')"></i>
                                                 </div>
                                                 <label class="error-message"></label>
@@ -202,7 +202,7 @@
                                             <fieldset>
                                                 <div class="form_g account">
                                                     <legend>手机号</legend>
-                                                    <input type="tel" placeholder="请输入11位手机号码" v-model="phoneNumber">
+                                                    <input type="tel" placeholder="请输入11位手机号码" v-model="phoneNumber"  class="telephone"  @input="checktelphone(phoneNumber,'telephone')"  maxlength="11">
                                                     <i class="close cs6" @click="ClearInput('cs6','phoneNumber')"></i>
                                                 </div>
                                                 <label class="error-message"></label>
