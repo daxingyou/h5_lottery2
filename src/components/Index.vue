a90c10589c89b620a207be885fc07da245f37385
Dashboard <wenziwenzi1719281685@gmail.com>
2018-02-02 14:15:24 +0800

    domain

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -288,7 +288,7 @@ export default {
       if(this.haslogin&& this.getCookie("acType")=='1' ){  // 只有登录状态才需要调
           this.getMsglistStatus()
       }   
-      // this.geturl() 
+      this.geturl() 
   },
     methods:{
       geturl:function(){
